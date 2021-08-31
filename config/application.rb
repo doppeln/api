60c34a8d59a625576096f2bacff5ec44e615c184
jiko797torayo <oec7dwayne7kj@gmail.com>
2021-09-01 07:38:38 +0900

    feat: RSpec導入

--- config/application.rb
+++ config/application.rb
@@ -21,20 +21,15 @@ Bundler.require(*Rails.groups)
 
 module App
   class Application < Rails::Application
-    # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: :true,
+        view_specs: :false,
+        helper_specs: :false,
+        routing_specs: :false
+      g.factory_bot false
+    end
   end
 end
